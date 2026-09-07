211110f5708563ba1259f1275bb515b0c61064f5
Sai Krishna Dandu <138280007+dandusaikrishna@users.noreply.github.com>
2026-09-07 23:52:44 +0530

    Remove OPTIONS method from CORS headers

--- http/middleware/cors.go
+++ http/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func EnableCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == "OPTIONS" {
